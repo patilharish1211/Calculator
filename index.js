f36a09d95491df7c76fa74cf0cd1bfcec6cdb56e
patilharish1211 <patilharish1211@gmail.com>
2025-07-10 12:12:08 +0530

    update

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-const keys = document.querySelectorAll("#keyboard>div");
-let store = "";
-let query = document.querySelector("#display");
+const display = document.querySelector('.display');
+const buttons = document.querySelectorAll('button');
 
-for (let i = 0; i < keys.length; i++) {
-  keys[i].addEventListener("click", (event) => {
-    let currentkey = event.target.innerText;
+let currentInput = '';
 
-    if (currentkey == "C") {
-      store = " ";
-      query.innerText = "";
-    } else if (currentkey == "=") {
-      query.innerText = eval(store);
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const value = button.textContent;
+
+    if (value === 'C') {
+      currentInput = '';
+    } else if (value === '=') {
+      try {
+        currentInput = eval(currentInput).toString();
+      } catch {
+        currentInput = 'Error';
+      }
     } else {
-      store += currentkey;
-      query.innerText = store;
+      currentInput += value;
     }
+
+    display.value = currentInput;
   });
-}
+});
